78eb87dd046f762c7614f9cc6747b39a8a76ce23
Miha1l <mr.loykonen@mail.ru>
2023-06-27 20:05:58 +0300

    refactor

--- src/reader.cpp
+++ src/reader.cpp
@@ -6,42 +6,38 @@
 #include <sys/shm.h>
 #include <sys/ipc.h>
 
-void printError() {
-    std::string s = std::to_string(errno);
-    std::perror(s.c_str());
+void printError(std::string s) {
+    std::string error = s + std::to_string(errno);
+    std::perror(error.c_str());
+}
+
+void deleteSegment(int shmId) {
+    if (shmctl(shmId, IPC_RMID, (struct shmid_ds *) 0) == -1) {
+        printError("[writer.cpp:shmctl] ");
+    }
 }
 
 int main() {
-    std::string keyString;
-    std::cin >> keyString;
     int shmKey;
-    try {
-        shmKey = std::stoi(keyString);
-    }
-    catch(std::invalid_argument e) {
-        std::cout << "Shared memory key is not a integer\n";
-        return 0;
-    }
-
+    std::cin >> shmKey;
     int shmId = shmget(shmKey, 1000, 0);
     if (shmId == -1) {
-        printError();
+        printError("[reader.cpp:shmget] ");
         return 0;
     }
 
     char* shmAddress = (char*)shmat(shmId, 0, 0);
     if (shmAddress == (char*)(-1)) {
-        printError();
+        printError("[reader.cpp:shmat] ");
+        deleteSegment(shmId);
         return 0;
     }
 
     std::cout << shmAddress << "\n";
     
     if (shmdt(shmAddress) == -1) {
-        printError();
+        printError("[reader.cpp:shmdt] ");
     }
-    if (shmctl(shmId, IPC_RMID, (struct shmid_ds *) 0) == -1) {
-        printError();
-    }
+    deleteSegment(shmId);
     return 0;
 }
--- src/writer.cpp
+++ src/writer.cpp
@@ -6,35 +6,39 @@
 #include <sys/shm.h>
 #include <sys/ipc.h>
 
-#define SHM_KEY 1112
+#define SHM_KEY 1111
 
-void printError() {
-    std::string s = std::to_string(errno);
-    std::perror(s.c_str());
+void printError(std::string s) {
+    std::string error = s + std::to_string(errno);
+    std::perror(error.c_str());
+}
+
+void deleteSegment(int shmId) {
+    if (shmctl(shmId, IPC_RMID, (struct shmid_ds *) 0) == -1) {
+        printError("[reader.cpp:shmctl] ");
+    }
 }
 
 int main() {
     int shmId = shmget(SHM_KEY, 1000, IPC_CREAT | IPC_EXCL | 0666);
     if (shmId == -1) {
-        printError();
+        printError("[writer.cpp:shmget] ");
         return 0;
     }
     
     char* shmAddress;
     shmAddress = (char*)shmat(shmId, 0, 0);
     if (shmAddress == (char*)(-1)) {
-        printError();
-        if (shmctl(shmId, IPC_RMID, (struct shmid_ds *) 0) == -1) {
-            printError();
-        }
+        printError("[writer.cpp:shmat] ");
+        deleteSegment(shmId);
         return 0;
     }
 
-    std::string str = "Hello World!";
-    std::strncpy(shmAddress, str.c_str(), str.size());
+    std::string s = "Hello World!";
+    std::strncpy(shmAddress, s.c_str(), s.size());
 
     if (shmdt(shmAddress) == -1) {
-        printError();
+        printError("[writer.cpp:shmdt] ");
     }
 
     std::cout << SHM_KEY << "\n";
